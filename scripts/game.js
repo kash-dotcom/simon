e2049b2a0899d939f7926f5f1174f8882201e04a
Katy Ashworth <katyashworth27@gmail.com>
2024-12-20 19:52:52 +0000

    Intial commit Add js, test.js, html, css, node, jest Implement game function to test file connectivity

---
+++ scripts/game.js
@@ -0,0 +1,12 @@
+const { choices } = require("yargs");
+
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1","button2","button3","button4"],
+};
+
+
+
+module.exports = { game };
---
+++ scripts/tests/game.test.js
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const { test, expect } = require("@jest/globals");
+const { game } = require ("../game.js");
+const exp = require("constants");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+    test("currentGame key exists", () => {
+        expect("currentGame" in game).toBe(true);
+    });
+    test("playerMoves key exists", () => {
+        expect("playerMoves" in game).toBe(true);
+    });
+    test("choices key exists", () => {
+        expect("choices" in game).toBe(true);
+    });
+    test("choices contain the correct ids", () => {
+        expect(game.choices).toEqual(["button1","button2","button3","button4"]);
+    });
+});
